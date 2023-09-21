7028de4dfd369b1f075f169215415af687919b87
Leah Jia <99011487+leahjia@users.noreply.github.com>
2023-09-20 21:54:03 -0700

    clear

--- src/main.rs
+++ src/main.rs
@@ -17,16 +17,10 @@ fn handle_connection(mut stream: TcpStream) {
     let mut buffer = [0; 1024];
     stream.read(&mut buffer).unwrap();
     let get = b"GET / HTTP/1.1\r\n";
+    let content = fs::read_to_string(if buffer.starts_with(get) { "index.html" } else { "404.html" }).unwrap();
+    let status = if buffer.starts_with(get) { "HTTP/1.1 200 OK" } else { "HTTP/1.1 404 NOT FOUND" };
 
-    if buffer.starts_with(get) {
-        let content = fs::read_to_string("index.html").unwrap();
-        let response = format! {"HTTP/1.1 200 OK\r\nContent-Length: {}\r\n\r\n{}", content.len(), content};
-        stream.write(response.as_bytes()).unwrap();
-        stream.flush().unwrap();
-    } else {
-        let content = fs::read_to_string("404.html").unwrap();
-        let response = format! {"{}\r\nContent-Length: {}\r\n\r\n{}", "HTTP/1.1 404 NOT FOUND", content.len(), content};
-        stream.write(response.as_bytes()).unwrap();
-        stream.flush().unwrap();
-    }
+    let response = format! {"{}\r\nContent-Length: {}\r\n\r\n{}", status, content.len(), content};
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
 }
