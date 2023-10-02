812b41e5fe313070e3fa57492670201357287f30
Leah Jia <99011487+leahjia@users.noreply.github.com>
2023-10-01 21:34:11 -0700

    thr pool

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -2,22 +2,35 @@ use std::fs;
 use std::io::prelude::*;
 use std::net::TcpListener;
 use std::net::TcpStream;
+use std::thread;
+use std::time::Duration;
+use server::ThreadPool;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    let pool = ThreadPool::new(4);
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
         println!("Connection established.");
-        handle_connection(stream);
+
+        pool.execute(|| {
+            handle_connection(stream);
+        });
     }
 }
 
 fn handle_connection(mut stream: TcpStream) {
     let mut buffer = [0; 1024];
     stream.read(&mut buffer).unwrap();
+
     let get = b"GET / HTTP/1.1\r\n";
+    let sleep = b"GET /sleep HTTP/1.1\r\n";
+
     let (file, status) = if buffer.starts_with(get) {
+        ("index.html", "HTTP/1.1 200 OK")
+    } else if buffer.starts_with(sleep) {
+        thread::sleep(Duration::from_secs(5));
         ("index.html", "HTTP/1.1 200 OK")
     } else {
         ("404.html", "HTTP/1.1 404 NOT FOUND")
